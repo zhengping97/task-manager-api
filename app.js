89420b7bf4f1d2a35b7a1e455d335c9da9c7f80a
hengzhengping <hengzhengping@gmail.com>
2022-09-10 16:20:56 +0800

    project setup

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+
+//middleware
+app.use(express.json())
+
+//routes
+app.get('/', (req, res) => {
+    res.send('Task Manager App')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+//app.get('/api/v1/tasks')          - get all the tasks
+//app.post('/api/v1/tasks')         - create a new task
+//app.get('/api/v1/tasks/:id')      - get single task
+//app.patch('/api/v1/tasks/:id')    - update task
+//app.delete('/api/v1/taska/:id')   - delete task
+
+const port = 3000 //http://localhost:3000/
+
+app.listen(port, console.log(`Server is listening on port ${port}...`))

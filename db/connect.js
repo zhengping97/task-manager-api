aca1af55735639c4602a21d1ac59850f3e0f05d3
hengzhengping <hengzhengping@gmail.com>
2022-09-16 17:44:50 +0800

    connect to database

---
+++ db/connect.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+//get string from database > connect > connect your application
+
+const connectDB = (url) => {
+    return mongoose.connect(url, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    })
+}
+
+module.exports = connectDB

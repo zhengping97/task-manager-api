0ef9650db9640a80ce18a532e5799b2d8075f8c3
hengzhengping <hengzhengping@gmail.com>
2022-09-17 20:41:25 +0800

    completed project

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ const app = express()
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
 //middleware
+app.use(express.static('./public'))
 app.use(express.json())
 
 //routes
-app.get('/', (req, res) => {
-    res.send('Task Manager App')
-})
-
 app.use('/api/v1/tasks', tasks)
+
+app.use(notFound)
+app.use(errorHandlerMiddleware)
 
 //app.get('/api/v1/tasks')          - get all the tasks
 //app.post('/api/v1/tasks')         - create a new task
